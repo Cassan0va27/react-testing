14dea2ce23f7d0a29bdb4342dbd1eecdf834e43d
Pushkar Mahajan <pushkarmahajan4@gmail.com>
2020-05-03 10:17:00 +0530

    Connected Netlify

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         My Awesome react App at learn code online
-        Axaxaxax
+        <p>Continous Integration</p>
       </header>
     </div>
   );
